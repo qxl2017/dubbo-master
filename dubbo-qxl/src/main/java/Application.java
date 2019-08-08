aefb7433db2a6e7038382161ef932dd2b6bc0e82
wanhuanchang <1109923832@qq.com>
2019-08-08 11:38:38 +0800

    first commit

---
+++ dubbo-qxl/src/main/java/Application.java
@@ -0,0 +1,9 @@
+import org.apache.dubbo.demo.DemoService;
+
+import java.util.UUID;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID());
+    }
+}
